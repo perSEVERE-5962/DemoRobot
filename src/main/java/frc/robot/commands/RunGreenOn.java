fca8ce4a0a2f5926835a1967317eb0494a1f2876
Unknown <pyash645@gmail.com>
2018-12-16 10:23:28 -0500

    Commit the Message

--- src/main/java/frc/robot/commands/RunGreenOn.java
+++ src/main/java/frc/robot/commands/RunGreenOn.java
@@ -2,13 +2,17 @@ package frc.robot.commands;
 
 import frc.robot.Robot;
 
+<<<<<<< refs/remotes/origin/2019
 
+=======
+>>>>>>> Commit the Message
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class RunGreenOn extends Command{
+<<<<<<< refs/remotes/origin/2019
     protected void initialize(){    
     }
 
@@ -18,6 +22,20 @@ public class RunGreenOn extends Command{
         NetworkTableEntry entry = table.getEntry("green");
         entry.setValue(100);
         Robot.oi.greenOn = true;
+=======
+  
+ 
+    protected void initialize(){
+        NetworkTableInstance inst =  NetworkTableInstance.getDefault();
+        NetworkTable table = inst.getTable("rgbled");
+        NetworkTableEntry entry = table.getEntry("green");
+        entry.setValue(255);
+        Robot.oi.greenOn = true;
+        
+    }
+
+    protected void execute(){
+>>>>>>> Commit the Message
     }
     protected boolean isFinished(){
         return (Robot.oi.greenOn == false);
